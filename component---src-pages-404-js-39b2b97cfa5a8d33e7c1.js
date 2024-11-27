4f6c4c6ee7b250c91cd53fde5127940edcd2326b
me-sajib <me-sajib@users.noreply.github.com>
2024-11-27 18:36:32 +0000

    deploy: f36018205906fe52d9c1d6f1c7f50629bbcedcf6

---
+++ component---src-pages-404-js-39b2b97cfa5a8d33e7c1.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_clean_resume=self.webpackChunkgatsby_starter_clean_resume||[]).push([[125],{3331:function(e,t,a){a.r(t);var r=a(6540),n=a(9765);t.default=()=>r.createElement(n.A,{pageTitle:"Page Not Found!",pageDescription:""},r.createElement("p",null,"Ooops! Wrong turn!"),r.createElement("p",null,"Head back to the ",r.createElement("a",{href:"/"},"Home")," page"))}}]);
+//# sourceMappingURL=component---src-pages-404-js-39b2b97cfa5a8d33e7c1.js.map
